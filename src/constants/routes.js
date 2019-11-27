30853426b9346512893f49a8e5f902f18158494f
jhadev <joshappeldev@gmail.com>
2019-11-27 10:54:54 -0500

    skeleton laid out, have to add pages and components

---
+++ src/constants/routes.js
@@ -0,0 +1,7 @@
+// define your routes here to uses throughout your app
+
+export const HOME = '/';
+export const SIGN_UP = '/signup';
+export const SIGN_IN = '/signin';
+
+// add more routes as you need them
